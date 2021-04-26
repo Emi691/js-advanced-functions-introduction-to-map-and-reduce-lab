b60ffe8bc1d34ba203c3f12d1028a7c2fc83bcb3
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-04-25 22:40:59 -0700

    commit tree e3cce1009ee2635472ceff1e457a5c7b6462b50d

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Your code here
+function mapToNegativize(sourceArray){
+    let newArray = []
+    sourceArray.forEach(element => newArray.push(0 - element))
+    return newArray
+}
+
+function mapToNoChange(sourceArray){
+    let newArray = []
+    sourceArray.forEach(element => newArray.push(element))
+    return newArray
+}
+
+function mapToDouble(sourceArray){
+    let newArray = []
+    sourceArray.forEach(element => newArray.push(element * 2))
+    return newArray
+}
+
+function mapToSquare(sourceArray){
+    let newArray = []
+    sourceArray.forEach(element => newArray.push(element ** 2))
+    return newArray
+}
+
+function reduceToTotal(sourceArray, startingPoint = 0){
+    let total = startingPoint
+    sourceArray.forEach(element => total = element + total)
+    return total
+}
+
+function reduceToAllTrue(sourceArray){
+    for(let i = 0; i < sourceArray.length; i++) {
+        if (!!sourceArray[i] === false){
+            return false
+        }
+    }
+    return true
+}
+
+function reduceToAnyTrue(sourceArray){
+    for(let i = 0; i < sourceArray.length; i++) {
+        if (!!sourceArray[i] === true){
+            return true
+        }
+    }
+    return false
+}
